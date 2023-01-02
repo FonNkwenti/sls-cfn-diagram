6a915e6eea2a1719f477fd81308376764ea1c90a
Fon Nkwent <fonnkwenti85@gmail.com>
2023-01-02 11:06:20 +0100

    dynamodb posts table, createPost handler, helper functions

--- src/handler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports.generateRandomNumber = (event) => {
-  const randomNumber = parseInt(Math.random() * 100);
-  console.log(`The random generated number is ${randomNumber} 🎉🎉🎉`);
-  return randomNumber;
-};
