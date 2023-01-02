44975085af83bbfe12a6acb017c1de0d2f13891f
Fon Nkwent <fonnkwenti85@gmail.com>
2023-01-02 15:33:42 +0100

    updating createPost PK

--- src/handlers/createPost.js
+++ src/handlers/createPost.js
@@ -8,12 +8,12 @@ const TableName = process.env.DYNAMODB_TABLE_NAME;
 const handler = async (event) => {
   try {
     const item = JSON.parse(event.body);
-    const id = randomUUID();
+    const postId = randomUUpostId();
     const now = new Date().toISOString();
-    const keySchema = { PK: "id" };
+    const keySchema = { PK: "postId" };
 
     let Item = {
-      [keySchema.PK]: id,
+      [keySchema.PK]: postId,
       ...item,
       createAt: now,
       updatedAt: now,
